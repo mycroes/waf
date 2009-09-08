678e47561699faa4b373fd6956b622d8dd8c88e6
Michael Croes <mycroes@Wind-U100.(none)>
2009-09-08 21:18:09 +0200

    Add Database class, no real code yet.

---
+++ src/library/Db.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @author Michael Croes <mycroes@gmail.com>
+ */
+
+/**
+ * Main database class. This class maintains a mysql connection and serves as
+ * a factory for tables classes.
+ */
+
+/**
+ * @package Db
+ */
+class Db {
+	
+	/**
+	 * Set up database connection.
+	 *
+	 */
+	public function __construct() {
+		
+	}
+}
